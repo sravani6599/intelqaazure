b755f47a8b8bc6a6eaf0a3d3e1ae6e112044e91b
sravani6599 <92807380+sravani6599@users.noreply.github.com>
2025-01-02 11:22:44 +0530

    Update variables.tf

--- terraform_azure/Layers/variables.tf
+++ terraform_azure/Layers/variables.tf
@@ -2,9 +2,11 @@ variable "location" {
   default = "East US"
 }
 variable "subscription_id" {
-  default = "489311d0-1d2f-45b2-a2d0-0cf9c09c85f6"
+ # default = "489311d0-1d2f-45b2-a2d0-0cf9c09c85f6"
+description = "your subscriptionid"
+
 }
 variable "tenant_id" {
-    default = "7571a489-bd29-4f38-b9a6-7c880f8cddf0"
-  
+   # default = "7571a489-bd29-4f38-b9a6-7c880f8cddf0"
+  description = "your tenant id"
 }
